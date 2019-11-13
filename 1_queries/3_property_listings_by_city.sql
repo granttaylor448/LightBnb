a72e21665e6a45f3bbfb39f53f98e5081d8257ed
granttaylor448 <granttaylor448@gmail.com>
2019-11-13 22:27:46 +0000

    adding 1_queries with 5 queries inside

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,8 @@
+SELECT properties.*, AVG(property_reviews.rating) AS average_rating
+FROM properties 
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE properties.city = 'Vancouver' AND property_reviews.rating >= 4
+GROUP BY properties.id
+ORDER BY cost_per_night
+LIMIT 10;
+
