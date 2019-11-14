4c2413cabff01af95dfde01c34b685a1938dbf25
granttaylor448 <granttaylor448@gmail.com>
2019-11-14 06:07:43 +0000

    added 3 pg functions to database.js

--- LightBnb_WebApp/server/database.js
+++ LightBnb_WebApp/server/database.js
@@ -18,16 +18,21 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  // let user;
+  // for (const userId in users) {
+    // user = users[userId];
+    // if (user.email.toLowerCase() === email.toLowerCase()) {
+      // break;
+    // } else {
+      // user = null;
+    // }
+  // }
+  return pool.query(`
+  SELECT users.* FROM users
+  WHERE users.email = $1
+  `, [email])
+   .then(res => (res.rows[0]))
+  
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -37,8 +42,15 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  // return Promise.resolve(users[id]);
+
+  return pool.query(`
+  SELECT users.* FROM users
+  WHERE users.id = $1
+  `, [id])
+  .then(res => (res.rows[0]));
 }
+
 exports.getUserWithId = getUserWithId;
 
 
@@ -48,10 +60,19 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // const values = [name, email, password]
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
+ 
+// }
+return pool.query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING users.id;
+  `,[user.name, user.email, user.password])
+  .then(res => (res.rows[0]));
 }
 exports.addUser = addUser;
 
